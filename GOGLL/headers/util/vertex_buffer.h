14f23944060be8208cd1203725b2f3717bbf59ae
Greffin28 <greffinroid@ymail.com>
2016-11-30 22:40:16 +0700

    Created vertex_buffer

---
+++ GOGLL/headers/util/vertex_buffer.h
@@ -0,0 +1,17 @@
+#ifndef _GOGLL_VERTEX_BUFFER_H_
+#define _GOGLL_VERTEX_BUFFER_H_
+
+#ifdef GOGLL_EXPORTS
+#define DLLExports __declspec(dllexport)
+#else
+#define DLLExports __declspec(dllimport)
+#endif
+
+namespace gogll {
+	class VertexBuffer {
+	private:
+	public:
+	};
+}
+
+#endif
