1d743223904e15087e70ff760d70cee8d84d879a
Greffin28 <greffinroid@ymail.com>
2017-01-10 20:36:49 +0700

    Image class added

---
+++ gogll/headers/util/Image.h
@@ -0,0 +1,17 @@
+#ifndef _GOGLL_IMAGE_H_
+#define _GOGLL_IMAGE_H_
+
+#ifdef GOGLL_EXPORTS
+#define DLLExports __declspec(dllexport)
+#else
+#define DLLExports __declspec(dllimport)
+#endif
+
+namespace gogll {
+	class Image {
+	private:
+	public:
+	};
+}
+
+#endif
